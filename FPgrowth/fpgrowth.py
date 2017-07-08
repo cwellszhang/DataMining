aeea5e3802ebf01f1d7269f41dcb194826987951
paradise6 <574327104@qq.com>
2017-07-08 22:39:29 +0800

    add recommendation
    recommendation parts

--- FPgrowth/fpgrowth.py
+++ FPgrowth/fpgrowth.py
@@ -27,6 +27,25 @@ def main(sc):
      # model_path =  "./fpm"
      # model.save(sc, model_path)
      # sameModel = FPGrowthModel.load(sc, model_path)
+     # recommendation part
+     userID=2
+     usrList=fpData.take(3)[userID]
+     print ("ID",userID,"shoppingList",usrList)
+     for i in model.freqItemsets().collect():
+         if i.items == usrList:
+             goodsFreq = i.freq
+     print ("GoodsNumber:",goodsFreq)
+
+     for i in model.freqItemsets().collect():
+         for k in usrList:
+          if k in i.items and len(i.items)>len(usrList):
+             conf  = float(i.freq)/float(goodsFreq)
+             if conf>=0.1:
+               item = i.items
+               for i in range(0,len(usrList)):
+                   if item not in usrList:
+                     print (item,"===",conf)
+
 
 if __name__ == "__main__":
     # Configure Spark
